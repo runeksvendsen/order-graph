eb3f1e1914f5564cb7a1e60542ad75e3652f4409
Rune Svendsen <rune@Runes-MacBook-Pro.local>
2021-12-04 18:04:16 +0100

    fix benchmark

--- bench/Main.hs
+++ bench/Main.hs
@@ -9,21 +9,24 @@ import qualified Control.Monad.ST as ST
 noLogging :: Monad m => String -> m ()
 noLogging = const $ return ()
 
-readBooks :: String -> Double -> IO [Lib.OrderBook Double]
-readBooks file slippage =
-    Lib.readOrdersFile noLogging (toRational slippage) file
+readBooks :: String -> IO [Lib.OrderBook Double]
+readBooks file =
+    Lib.readOrdersFile noLogging file
 
-buildMatch :: Lib.Currency -> Lib.Currency -> [Lib.OrderBook Double] -> ([Lib.SellPath], [Lib.BuyPath])
-buildMatch numeraire crypto orderBooks =
-    ST.runST $ Lib.buildBuyGraph noLogging orderBooks >>=
+buildMatch :: Lib.Currency -> Lib.Currency -> Rational -> [Lib.OrderBook Double] -> ([Lib.SellPath], [Lib.BuyPath])
+buildMatch numeraire crypto maxSlippage orderBooks =
+    ST.runST $ Lib.buildBuyGraph noLogging maxSlippage orderBooks >>=
         Lib.matchOrders noLogging numeraire crypto . snd
 
 main :: IO ()
 main = do
-    books <- readBooks "test/data/double/test19.json" 0.5
+    books <- readBooks "test/data/double/test19.json"
     Gauge.defaultMain [
         Gauge.bgroup "fib"
             [ Gauge.Benchmark "test" $
-                Gauge.whnf (buildMatch "BTC" "USD") books
+                Gauge.whnf (buildMatch "BTC" "USD" (toRational maxSlippage)) books
             ]
         ]
+  where
+    maxSlippage :: Double
+    maxSlippage = 0.5
